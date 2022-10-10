8cc9b282cc4f5cb2461c1981e9089f26c422b324
Matthew <matthewtoro17@gmail.com>
2022-10-10 19:26:34 +0800

    Create function to get possible moves

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,4 +1,28 @@
 const squareFactory = (x, y) => {
-  let moves = [];
+  let moves = getPossibleMoves();
+
+  function isValidMove(x, y) {
+    return x > -1 && x < 8 && y > -1 && y < 8;
+  };
+
+  function getPossibleMoves() {
+    let output = [];
+    const direction = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]];
+
+    direction.forEach(d => {
+      let newX = x + d[0];
+      let newY = y + d[1];
+
+      if (isValidMove(newX, newY)) {
+        output.push([newX, newY]);
+      }
+    });
+
+    return output;
+  };
+
   return { moves };
 };
+
+let square = squareFactory(3, 0);
+console.log(square.moves);
